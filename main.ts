3b139b8a28b980578fbc50783db9889b06ab2909
cool-guys-bfc2 <lamarasingham@icloud.com>
2025-12-29 11:50:27 -0500

    Add WebSocket support and HTTP request handling

--- main.ts
+++ main.ts
@@ -12,7 +12,32 @@ app.get("/api2/:name", (ctx) => {
     `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}!`,
   );
 });
+Deno.serve({
+  port: 8080,
+  async handler(request) {
+    if (request.headers.get("upgrade") !== "websocket") {
+      // If the request is a normal HTTP request,
+      // we serve the client HTML file.
+      const file = await Deno.open("./index.html", { read: true });
+      return new Response(file.readable);
+    }
+    // If the request is a websocket upgrade,
+    // we need to use the Deno.upgradeWebSocket helper
+    const { socket, response } = Deno.upgradeWebSocket(request);
 
+    socket.onopen = () => {
+      console.log("CONNECTED");
+    };
+    socket.onmessage = (event) => {
+      console.log(`RECEIVED: ${event.data}`);
+      socket.send("pong");
+    };
+    socket.onclose = () => console.log("DISCONNECTED");
+    socket.onerror = (error) => console.error("ERROR:", error);
+
+    return response;
+  },
+});
 // this can also be defined via a file. feel free to delete this!
 const exampleLoggerMiddleware = define.middleware((ctx) => {
   console.log(`${ctx.req.method} ${ctx.req.url}`);
